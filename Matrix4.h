c7493c2d30904266a2abf42eb5e271b4081f1654
JacobDYG <thejacobbennett0@gmail.com>
2022-01-19 15:10:25 +0000

    Added convinence methods to Matrix4 for getting transformation matrices

--- Matrix4.h
+++ Matrix4.h
@@ -30,69 +30,73 @@ class Matrix4;
 // this allows us to get a matrix in the 
 // column-major form preferred by OpenGL
 class columnMajorMatrix
-    { // class columnMajorMatrix
-    public:
-    float coordinates[16];
-    }; // class columnMajorMatrix
-    
+	{ // class columnMajorMatrix
+	public:
+	float coordinates[16];
+	}; // class columnMajorMatrix
+	
 // the class itself, stored in row-major form
 class Matrix4
-    { // Matrix4
-    public:
-    // the coordinates
-    float coordinates[4][4];
+	{ // Matrix4
+	public:
+	// the coordinates
+	float coordinates[4][4];
 
-    // constructor - default to the zero matrix
-    Matrix4();
-    // copy constructor
-    Matrix4(const Matrix4 &other);
-    
-    // equality operator
-    bool operator ==(const Matrix4 &other) const;
+	// constructor - default to the zero matrix
+	Matrix4();
+	// copy constructor
+	Matrix4(const Matrix4 &other);
+	
+	// equality operator
+	bool operator ==(const Matrix4 &other) const;
 
-    // indexing - retrieves the beginning of a line
-    // array indexing will then retrieve an element
-    float * operator [](const int rowIndex);
-    
-    // similar routine for const pointers
-    const float * operator [](const int rowIndex) const;
+	// indexing - retrieves the beginning of a line
+	// array indexing will then retrieve an element
+	float * operator [](const int rowIndex);
+	
+	// similar routine for const pointers
+	const float * operator [](const int rowIndex) const;
 
-    // scalar operations
-    // multiplication operator (no division operator)
-    Matrix4 operator *(float factor) const;
+	// scalar operations
+	// multiplication operator (no division operator)
+	Matrix4 operator *(float factor) const;
 
-    // vector operations on homogeneous coordinates
-    // multiplication is the only operator we use
-    Homogeneous4 operator *(const Homogeneous4 &vector) const;
+	// vector operations on homogeneous coordinates
+	// multiplication is the only operator we use
+	Homogeneous4 operator *(const Homogeneous4 &vector) const;
 
-    // and on Cartesian coordinates
-    Cartesian3 operator *(const Cartesian3 &vector) const;
+	// and on Cartesian coordinates
+	Cartesian3 operator *(const Cartesian3 &vector) const;
 
-    // matrix operations
-    // addition operator
-    Matrix4 operator +(const Matrix4 &other) const;
-    // subtraction operator
-    Matrix4 operator -(const Matrix4 &other) const;
-    // multiplication operator
-    Matrix4 operator *(const Matrix4 &other) const; 
-    
-    // matrix transpose
-    Matrix4 transpose() const;
-    
-    // returns a column-major array of 16 values
-    // for use with OpenGL
-    columnMajorMatrix columnMajor() const;
+	// matrix operations
+	// addition operator
+	Matrix4 operator +(const Matrix4 &other) const;
+	// subtraction operator
+	Matrix4 operator -(const Matrix4 &other) const;
+	// multiplication operator
+	Matrix4 operator *(const Matrix4 &other) const; 
+	
+	// matrix transpose
+	Matrix4 transpose() const;
+	
+	// returns a column-major array of 16 values
+	// for use with OpenGL
+	columnMajorMatrix columnMajor() const;
 
-    // methods that set to particular matrices
-    void SetZero();
-    // the identity matrix
-    void SetIdentity();
-    void SetTranslation(const Cartesian3 &vector);
-    void SetRotation(const Cartesian3 &axis, float theta);
-    void SetScale(float xScale, float yScale, float zScale);
+	// methods that set to particular matrices
+	void SetZero();
+	// the identity matrix
+	void SetIdentity();
+	void SetTranslation(const Cartesian3 &vector);
+	void SetRotation(const Cartesian3 &axis, float theta);
+	void SetScale(float xScale, float yScale, float zScale);
 
-    const static Matrix4 Identity();
-    }; // Matrix4
+	const static Matrix4 Identity();
+	const static Matrix4 TranslationMultMat(Cartesian3 translation);
+	const static Matrix4 ScaleMultMat(float xScale, float yScale, float zScale);
+	const static Matrix4 ScaleMultMat(float scale) { return ScaleMultMat(scale, scale, scale); };
+	const static Matrix4 RotationMultMat(const Cartesian3 axis, float theta);
+	}; // Matrix4
 
 // scalar operations
 // additional scalar multiplication operator
@@ -103,5 +107,5 @@ std::istream & operator >> (std::istream &inStream, Matrix4 &value);
 
 // stream output
 std::ostream & operator << (std::ostream &outStream, const Matrix4 &value);
-        
+		
 #endif
